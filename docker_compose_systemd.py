786f037df33e0752b3441b957ec894496678b39c
Nadrieril <nadrieril@gmail.com>
2015-06-27 20:10:08 +0200

    Renamed some variables to avoid confusion

--- docker_compose_systemd.py
+++ docker_compose_systemd.py
@@ -10,19 +10,21 @@ Restart=always
 RestartSec=10s
 ExecStartPre=-/usr/bin/docker kill {container_name}
 ExecStartPre=-/usr/bin/docker rm {container_name}
-ExecStart=/usr/bin/docker run --rm --name {container_name} {args} {image}
+ExecStart=/usr/bin/docker run --rm --name "{container_name}" \\
+        {args} \\
+        {image}
 ExecStop=/usr/bin/docker stop {container_name}
 ExecStopPost=-/usr/bin/docker rm {container_name}
 
 [Install]
-WantedBy=multi-user.target"""
+WantedBy=multi-user.target
+"""
 
-DEPENDENCY_TEMPLATE = """After={service}
-Requires={service}"""
+DEPENDENCY_TEMPLATE = "After={unit}\nRequires={unit}"
 
-IMG_NAME = "{service}-{container}"
-CTNR_NAME = "{service}-{container}-1"
-UNIT_NAME = "{service}-{container}.docker-compose.service"
+IMG_NAME = "{project}-{service}"
+CTNR_NAME = "{project}-{service}-1"
+UNIT_NAME = "{project}-{service}.docker-compose.service"
 
 
 DOCKER_CONFIG_KEYS = [
@@ -119,17 +121,17 @@ def _mount_volumes_at_path(project, volume_mount_path):
             mount_dir = os.path.join(volume_mount_path, dir_name)
             config['volumes'][i] = "%s:%s" % (mount_dir, vol)
 
-def _generate_units(service_name, project):
+def _generate_units(project_name, project):
     def convert_option_to_param(opt, value):
         param = '--' + opt.replace('_', '-')
         param = KEYS_OPTION_MAP.get(opt, param)
-        return "%s %s" % (param, value)
+        return '%s "%s"' % (param, value)
 
     units = {}
-    for container, config in project.items():
-        outfile = UNIT_NAME.format(**{'service': service_name, 'container': container})
-        container_name = CTNR_NAME.format(**{'service': service_name, 'container': container})
-        default_image = IMG_NAME.format(**{'service': service_name, 'container': container})
+    for service_name, config in project.items():
+        outfile = UNIT_NAME.format(project=project_name, service=service_name)
+        container_name = CTNR_NAME.format(project=project_name, service=service_name)
+        default_image = IMG_NAME.format(project=project_name, service=service_name)
 
         if 'image' in config and 'build' in config:
             raise Exception('Cannot specify both \'image\' and \'build\' options')
@@ -138,7 +140,7 @@ def _generate_units(service_name, project):
         image = None
         for k, v in config.items():
             if k not in ALLOWED_KEYS:
-                print "Unsupported option '%s' in container '%s' definition. Ignoring" % (k, container)
+                print "Unsupported option '%s' in service '%s' definition. Ignoring" % (k, service_name)
                 continue
 
             if k == 'image':
@@ -151,8 +153,6 @@ def _generate_units(service_name, project):
             else:
                 args.append(convert_option_to_param(k, v))
 
-        print container + ':', ' '.join(args)
-
 
         dependencies = set()
         if 'volumes_from' in config:
@@ -163,25 +163,27 @@ def _generate_units(service_name, project):
             dependencies.add(config['net'].split(':')[-1])
 
         if len(dependencies) != 0:
-            dependencies = [UNIT_NAME.format(service=service_name, container=dep) for dep in dependencies]
-            dependencies = '\n'.join(DEPENDENCY_TEMPLATE.format(service=dep) for dep in dependencies)
+            dependencies = [UNIT_NAME.format(project=project_name, service=dep) for dep in dependencies]
+            dependencies = '\n'.join(DEPENDENCY_TEMPLATE.format(unit=dep) for dep in dependencies)
         else:
-            dependencies = DEPENDENCY_TEMPLATE.format(service='docker.service')
+            dependencies = DEPENDENCY_TEMPLATE.format(unit='docker.service')
 
-        units[outfile] = UNIT_TEMPLATE.format(container_name=container_name, image=image, args=' '.join(args), dependencies=dependencies)
+        units[outfile] = UNIT_TEMPLATE.format(project=project_name, service=service_name, \
+                                                container_name=container_name, image=image, \
+                                                args=' '.join(args), dependencies=dependencies)
 
     return units
 
 if __name__ == '__main__':
     filename = "test/docker-compose.yml"
     project = _load_yaml(filename)
-    service_name = 'test'
+    project_name = 'test'
     override = {}
 
     _check_host_dependencies(project)
     _combine_project_override(project, override)
-    _mount_volumes_at_path(project, '/srv/%s' % service_name)
-    for filename, content in _generate_units(service_name, project).items():
+    _mount_volumes_at_path(project, '/srv/%s' % project_name)
+    for filename, content in _generate_units(project_name, project).items():
         with open("out/" + filename, 'w') as f:
             f.write(content)
 
